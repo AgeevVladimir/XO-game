69745d740fef8d17e92ba0c8098bd515a02f46ad
VladimirAgeev <ageev.v.s@gmail.com>
2020-10-14 01:20:15 +0300

    Field updated

---
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -0,0 +1,35 @@
+package io.hexlet.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FieldTest {
+
+    @Test
+    void getSize() {
+
+        final int expectedValue = 3;
+
+        final Field field = new Field();
+
+        assertEquals(expectedValue,field.getSize());
+    }
+
+    @Test
+    void setFigure() {
+
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputFigure,actualFigure);
+
+    }
+}
