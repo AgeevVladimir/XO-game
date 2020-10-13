4925c4ce0948c769098284361e8db98a9b4cb171
VladimirAgeev <ageev.v.s@gmail.com>
2020-10-14 00:23:45 +0300

    PlayerTest created

---
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,36 @@
+package io.hexlet.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+
+        final String inputValue = "Vovan";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void getFigure() {
+
+        final Figure inputValue = Figure.O;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+
+    }
+}
