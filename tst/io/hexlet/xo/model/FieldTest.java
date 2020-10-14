1b3b2f8d1a97e52da883b71112ffc2c1e9d02dfb
VladimirAgeev <ageev.v.s@gmail.com>
2020-10-15 00:38:39 +0300

    Task-3 SetFigure AlreadyOccupiedException added

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,7 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -19,18 +21,84 @@ public class FieldTest {
     }
 
     @Test
-    void setFigure() {
+    void setFigure() throws InvalidPointException, AlreadyOccupiedException {
 
         final Field field = new Field();
         final Point inputPoint = new Point(0,0);
         final Figure inputFigure = Figure.O;
-        final Figure expectedFigure = inputFigure;
 
         field.setFigure(inputPoint, inputFigure);
 
         final Figure actualFigure = field.getFigure(inputPoint);
 
-        assertEquals(expectedFigure,actualFigure);
+        assertEquals(inputFigure,actualFigure);
 
+    }
+
+    @Test
+    void getFigureWhenFigureIsNotSetted() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    void getFigureWhenXIsLessThenZero() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void getFigureWhenYIsLessThenZero() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-2);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void getFigureWhenXIsMoreThenSize() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize()+1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void getFigureWhenYIsLessThenSize() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,field.getSize()+1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void setFigureWhenAlreadyOcupied () throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
     }
 }
