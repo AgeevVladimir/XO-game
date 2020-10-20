1ef283ce295fb4c2341df19b36aee9fc21aa817e
VladimirAgeev <ageev.v.s@gmail.com>
2020-10-21 00:32:18 +0300

    Task-5 WinnerController refactored + lambda

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -11,17 +11,17 @@ public class WinnerController {
     public Figure getWinner(final Field field) throws InvalidPointException {
         try {
             for (int i = 0; i < 3; i++)
-            if (check(field, new Point(i,0), new Point(i,1), new Point(i,2)))
-                return field.getFigure(new Point(i,0));
+                if (check(field, new Point(i,0), p -> new Point(p.x, p.y + 1)))
+                    return field.getFigure(new Point(i,0));
 
             for (int i = 0; i < 3; i++)
-                if (check(field, new Point(0,i), new Point(1,i), new Point(2,i)))
+                if (check(field, new Point(0,i), p -> new Point(p.x + 1, p.y)))
                     return field.getFigure(new Point(0,i));
 
-            if (check(field, new Point(0,0), new Point(1,1), new Point(2,2)))
+            if (check(field, new Point(0,0), p -> new Point(p.x + 1, p.y + 1)))
                 return field.getFigure(new Point(1,1));
 
-            if (check(field, new Point(2,0), new Point(1,1), new Point(0,2)))
+            if (check(field, new Point(0,2), p -> new Point(p.x + 1, p.y - 1)))
                 return field.getFigure(new Point(1,1));
 
         } catch (InvalidPointException e) {
@@ -30,17 +30,31 @@ public class WinnerController {
         return null;
     }
 
-    private boolean check(final Field field, final Point p1, final Point p2, final Point p3) throws InvalidPointException {
-        try {
-            if (field.getFigure(p1) == null) return false;
-            if (field.getFigure(p1) == field.getFigure(p2) &&
-                    field.getFigure(p2) == field.getFigure(p3))
-                return true;
-        } catch (InvalidPointException e) {
-            e.printStackTrace();
-        }
-        return false;
+    private boolean check(final Field field,
+                          final Point currentPoint,
+                          final IPointGenerator pointGenerator) throws InvalidPointException {
+        final Figure currentFigure;
+        final Figure nextFugure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
 
+        try {
+            currentFigure = field.getFigure(currentPoint);
+            nextFugure = field.getFigure(nextPoint);
+        } catch (final InvalidPointException e) {
+            return true;
+        }
+
+        if (currentFigure == null) return false;
+
+        if (currentFigure != nextFugure) return false;
+
+        return check(field, nextPoint, pointGenerator);
+
+        }
+
+    private interface  IPointGenerator {
+
+        public Point next(final Point point);
     }
 
 }
