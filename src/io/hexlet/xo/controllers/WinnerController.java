2cecebf967308f7b350e3fa9df705f468f355764
VladimirAgeev <ageev.v.s@gmail.com>
2020-10-20 23:03:49 +0300

    Task-4 WinnerController refactoring

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -10,19 +10,37 @@ public class WinnerController {
 
     public Figure getWinner(final Field field) throws InvalidPointException {
         try {
-            if (field.getFigure(new Point(0, 0)) == field.getFigure(new Point(0, 1)) &&
-                    field.getFigure(new Point(0, 1)) == field.getFigure(new Point(0, 2)))
-                return field.getFigure(new Point(0, 0));
-            if (field.getFigure(new Point(1, 0)) == field.getFigure(new Point(1, 1)) &&
-                    field.getFigure(new Point(1, 1)) == field.getFigure(new Point(1, 2)))
-                return field.getFigure(new Point(1, 1));
-            if (field.getFigure(new Point(2, 0)) == field.getFigure(new Point(2, 1)) &&
-                    field.getFigure(new Point(2, 1)) == field.getFigure(new Point(2, 2)))
-                return field.getFigure(new Point(2, 2));
+            for (int i = 0; i < 3; i++)
+            if (check(field, new Point(i,0), new Point(i,1), new Point(i,2)))
+                return field.getFigure(new Point(i,0));
+
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(0,i), new Point(1,i), new Point(2,i)))
+                    return field.getFigure(new Point(0,i));
+
+            if (check(field, new Point(0,0), new Point(1,1), new Point(2,2)))
+                return field.getFigure(new Point(1,1));
+
+            if (check(field, new Point(2,0), new Point(1,1), new Point(0,2)))
+                return field.getFigure(new Point(1,1));
+
         } catch (InvalidPointException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private boolean check(final Field field, final Point p1, final Point p2, final Point p3) throws InvalidPointException {
+        try {
+            if (field.getFigure(p1) == null) return false;
+            if (field.getFigure(p1) == field.getFigure(p2) &&
+                    field.getFigure(p2) == field.getFigure(p3))
+                return true;
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return false;
+
     }
 
 }
