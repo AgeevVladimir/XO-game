8c566594493ce894193ac0e82141160782839c53
VladimirAgeev <ageev.v.s@gmail.com>
2020-10-25 23:34:52 +0300

    Task-6 CurrentMoveController refactored

--- src/io/hexlet/xo/controllers/CurrentMoveController.java
+++ src/io/hexlet/xo/controllers/CurrentMoveController.java
@@ -12,29 +12,23 @@ import static io.hexlet.xo.model.Figure.X;
 public class CurrentMoveController {
 
     public Figure currentMove(final Field field) {
-        int countX = 0;
-        int countO = 0;
+        int countFigure = 0;
         for (int x=0; x < field.getSize(); x++) {
             for (int y = 0; y < field.getSize(); y++) {
                 try {
-                    switch (field.getFigure(new Point(x,y))) {
-                        case O:
-                            countO++;
-                            break;
-                        case X:
-                            countX++;
-                            break;
+                    if (field.getFigure(new Point(x,y)) != null) {
+                        countFigure++;
                     }
                 } catch (InvalidPointException e) {
                     e.printStackTrace();
                 }
             }
         }
-        if (countO == countX)
-            return Figure.X;
-
-        if (countO + countX == field.getSize() * field.getSize())
+        if (countFigure == field.getSize() * field.getSize())
             return null;
+
+        if (countFigure % 2 == 0)
+            return Figure.X;
 
         return Figure.O;
 
