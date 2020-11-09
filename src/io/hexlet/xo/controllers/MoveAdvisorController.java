8bb1978fabbe749156ee7907408d3975c3a65fa1
VladimirAgeev <ageev.v.s@gmail.com>
2020-11-09 17:58:10 +0300

    Task-10 MoveAdvisorController finalized

--- src/io/hexlet/xo/controllers/MoveAdvisorController.java
+++ src/io/hexlet/xo/controllers/MoveAdvisorController.java
@@ -1,13 +1,28 @@
 package io.hexlet.xo.controllers;
 
 import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class MoveAdvisorController {
 
-    public Point[] getAllAvailablePoints(final Field field){
+    public Point[] getAllAvailablePoints(final Field field)
+            throws InvalidPointException {
 
+        int i = 0;
+        final Point[] availablePoints = new Point[field.getSize()];
+        for (int x = 0; x < field.getSize(); x++) {
+            for (int y = 0; y < field.getSize(); y++) {
+                Point point = new Point(x, y);
+                if (field.getFigure(point) != null) {
+
+                    availablePoints[i] = point;
+                    i++;
+                }
+            }
+        }
+        return null;
 
     }
 
